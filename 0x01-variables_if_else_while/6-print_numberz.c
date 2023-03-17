968dcbd2f0d73ab773e0f1168b330512140a5542
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-17 17:03:05 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Print all single digit numbers of base 10 using putchar
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
