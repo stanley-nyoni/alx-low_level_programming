0b045a98141614da44031a9f12f62aa83a9f2d9a
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-17 15:45:56 +0000

    Print the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+	putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
