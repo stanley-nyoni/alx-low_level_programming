a4a9ce61f744caabe3405657822851270b0785b5
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-21 09:42:36 -0700

    Program that prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -36,7 +36,7 @@ int main(void)
 			}
 			fn2 = (n11 + n22);
 			fn = n1 + n2 + (fn2 / 1000000000);
-			printf(, %ld", fn);
+			printf( , %ld", fn);
 			printf(%ld", fn2 % 1000000000);
 			n1 = n2;
 			n11 = n22;
