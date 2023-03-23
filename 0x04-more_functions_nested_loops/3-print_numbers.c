b130d7616d4ce494c69c1ac27dc44689c048705b
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-23 11:08:42 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers, from 0 - 9
+*
+* Retun: void.
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
