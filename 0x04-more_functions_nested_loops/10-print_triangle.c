bf9ef3e94734d0e917b718c6d584a1e327da8c47
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-23 13:49:52 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_triangle - prints a triangle
+*
+*@size: number of lines
+*
+* Return: void
+*/
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - 1); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
