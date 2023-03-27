a8999daa9f4311dee821958bc76f7c4e6d255aa5
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-27 18:57:37 +0000

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - updates the value of n
+*@n: input integer
+*
+*Return: void.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Welcome to C - Pointers, arrays and strings.
