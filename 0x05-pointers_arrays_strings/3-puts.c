433c780b4fb386a4eeece2520bf2a0372143a534
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-27 19:33:33 +0000

    Func that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _puts - prints the string
+* @str: input string
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+		i++;
+	}
+}
