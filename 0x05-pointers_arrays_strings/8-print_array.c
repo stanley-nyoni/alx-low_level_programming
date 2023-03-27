5209ff3995be4380d5c35ab3c68e2e5c06abaaa3
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-27 21:10:40 +0000

    Func that prints elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_array - prints the elements of an array of integers
+* @a: input array
+* @n: input number of elements
+*
+* Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ";
+	}
+	printf("\n");
+}
