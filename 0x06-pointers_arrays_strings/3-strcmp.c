5bb83b762a3647a87189d39bab95a535e837c952
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-03-29 20:36:28 +0000

    Func that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,25 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 	int flag = 0;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	while (flag == 0)
 	{
-		if (s1[i] != s2[i] && s1[i] >= s2[i])
-		{
-			flag = 15;
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		}
-		else if (s1[i] != s2[i] && s1[i] <= s2[i])
-		{
-			flag = -15;
-			break;
-		}
-		else
-		{
-			flag = 0;
-		}
+		flag = *(s1 + i) - *(s2 + i);
+		i++;
 	}
 	return (flag);
 }
