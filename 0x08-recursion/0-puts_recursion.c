fae68bccc36ba61e6afc245eff5f9af8c3d968b6
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-05 18:38:41 +0000

    Func that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints a string
+* @s: input string
+*
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
