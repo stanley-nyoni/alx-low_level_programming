1b43bf914f7c765393611a73f5c7f0c3d78761c6
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-11 19:51:23 +0000

    Copy a string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strdup - copy the string given as a parameter
+*@str: input string to be copied
+*
+*Return: pointer to duplicate string
+*/
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *copy;
+
+	j = 0;
+	while (*(str + j) != '\0')
+	{
+		j++;
+	}
+
+	copy = (char *) malloc((sizeof(char)) * j);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < j; i++)
+	{
+		*(copy + i) = *(str + i);
+	}
+	return (copy);
+	free(copy);
+}
