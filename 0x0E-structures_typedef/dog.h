e71b36fa3b12a51680335302e035c963fba8d71b
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-17 19:12:40 +0000

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+* struct dog - define elements of my dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
