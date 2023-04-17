bb07920ec24ba683eb778e342d3a73864faaf20e
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-17 21:29:19 +0000

    Typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
+* dog_t - Typedef for struct dog
+*/
+
+typedef struct dog dog_t;
+
+
+/**
 * struct dog - define elements of my dog
 *@name: name of dog
 *@age: age of dog
