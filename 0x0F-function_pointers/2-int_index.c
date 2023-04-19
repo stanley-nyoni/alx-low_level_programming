5f13fc59f8bb2765dc3652973af15bfafbe4ce7e
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-19 21:11:18 +0000

    Searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,46 @@
+#include "function_pointers.h"
+
+/**
+* int_index - searches for an index
+*@array: array of elements
+*@size: size of the array of elements
+*@cmp: pointer to a function that takes an int and returns int
+*
+* Return: -1 if size <= 0, or if no match.
+* index of element if matches
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int flag = 0;
+	int rtn = 0;
+
+	if (array && size && cmp)
+	{
+
+		if (size <= 0)
+		{
+			rtn = -1;
+		}
+
+		for (i = 0; i < size; i++)
+		{
+			flag = cmp(array[i]);
+			if (flag != 0)
+			{
+				rtn = i;
+				break;
+			}
+			else
+			{
+				rtn = 0;
+			}
+		}
+		if (flag == 0)
+		{
+			rtn = -1;
+		}
+	}
+	return (rtn);
+}
