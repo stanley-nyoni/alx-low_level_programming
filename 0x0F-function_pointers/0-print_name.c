c38ab8242885954a125a5d241a117a8305a0247c
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-19 18:58:22 +0000

    Func that prints name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name
+*@name: name to be printed
+*@f: call back function that prints name
+*
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
