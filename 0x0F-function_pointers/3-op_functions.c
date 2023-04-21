fd55f42ec971ec249b9c058abfaa5908a12caf2d
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-04-21 19:06:43 +0000

    Corrections task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+* op_add - add two int
+*@a: int 1
+*@b: int 2
+*
+* Return: sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - subracts b from a
+*@a: int 1
+*@b: int 2
+*
+* Return: difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - multiplies 2 int
+*@a: int 1
+*@b: int 2
+*
+* Return: product
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* op_div - divides 2 int
+*@a: int 1
+*@b: int 2
+*
+* Return: division
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - get modulus of 2 int
+*@a: int 1
+*@b: int 2
+*
+* Return: remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
