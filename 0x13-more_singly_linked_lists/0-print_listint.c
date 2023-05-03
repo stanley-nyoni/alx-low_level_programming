31e36a55e53f827dd7c3187f537e93a4365db930
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-05-03 13:38:11 +0000

    Func that prints all elements in a lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,8 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	if (h == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
