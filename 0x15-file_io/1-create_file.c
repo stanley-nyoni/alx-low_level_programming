56f3c613184ecc94a37ef66e73241c16f4ca016a
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-07-10 18:22:51 +0000

    Func that creats a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
 	if (fd < 0)
 		return (-1);
 
