2bc758da3fc84fb18e1e61baeb07d947212d2bb5
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-07-10 18:16:17 +0000

    Func that creats a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* create_file - creates a file
+* @filename: pointer to the file name
+* @text_content: string to be written into the new file
+*
+* Return: 1 on success,
+* -1 on failure(can't create file,write fails)
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len;
+	ssize_t w_chars;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	len = strlen(text_content);
+	w_chars = write(fd, text_content, len);
+	if (w_chars < 0)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
