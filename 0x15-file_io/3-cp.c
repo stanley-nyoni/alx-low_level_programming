5cc68cf15be6388949f7c673efec37dae976b14d
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-07-11 18:05:41 +0000

    Copy file contents to another file - bug fix 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,13 +12,13 @@ void print_err(int fd[], char **av)
 {
 	if (fd[0] < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 
 	if (fd[1] < 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 
@@ -40,7 +40,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -51,24 +51,24 @@ int main(int ac, char **av)
 	if (r_chars < 0)
 	{
 		close(fd[0]);
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	w_chars = write(fd[1], buff, r_chars);
 	if (w_chars < 0 && w_chars != r_chars)
 	{
 		close(fd[1]);
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	if (close(fd[0]) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd[0]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd[0]);
 		exit(100);
 	}
 	if (close(fd[1]) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd[1]);
 		exit(100);
 	}
 	return (0);
