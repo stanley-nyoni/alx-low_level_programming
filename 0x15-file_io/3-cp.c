1f358db92887d8031cca3b97f2cef265e0796b1c
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-07 21:04:56 +0000

    a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,72 @@
+#include "main.h"
+
+/**
+* print_err - prints the error message
+* @file_from: file from
+* @file_to: file to
+* @av: array of arguments
+*
+* Return: void
+*/
+
+void print_err(int file_from, int file_to, char **av)
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+* main - entry point
+* @ac: args count
+* @av: array of args
+*
+* Return: 0
+*/
+
+int main(int ac, char **av)
+{
+	int file_from, file_to, close_err;
+	ssize_t r_chars, w_chars;
+	char buf[1024];
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open(av[1], O_RDONLY);
+	file_to = open(av[2], O_CREAT | O_WROLY | O_TRUCN | O_APPEND, 0664);
+	print_err(file_from, file_to, av);
+
+	r_chars = 1024;
+	while (r_chars == 1024)
+	{
+		r_chars = read(file_from, buf, 1024);
+		if (r_chars < 0)
+			print_err(-1, 0, av);
+		w_chars = write(file_to, buf, r_chars);
+		if (w_chars < 0)
+			print_err(0, -1, av);
+	}
+	close_err = close(file_from);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	close_err = close(file_to);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
