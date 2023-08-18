c761eb00ed7696b879422048ea6488a50cde1908
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-18 18:37:26 +0000

    number of elements in  a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* dlistint_len - returns the number of elements in  a list
+* @h: head pointer
+*
+* Return: number of elements
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
