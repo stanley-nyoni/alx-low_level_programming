d59ba280ed85240c1481e71dd608d193c81cec1c
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-18 22:41:30 +0000

    insert node at given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+* insert_dnodeint_at_index - insert node at a given position
+* @idx: index
+* @n: integer
+* @h: head pointer
+*
+* Return: address of new node
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = NULL;
+	unsigned int count = 0;
+	dlistint_t *p, *ptr;
+	unsigned int i = 1;
+
+	p = *h;
+	while (p != NULL)
+	{
+		p = p->next;
+		count++;
+	}
+
+	if (idx > count)
+	{
+		return (NULL);
+	}
+	else
+	{
+		temp = (dlistint_t *)malloc(sizeof(dlistint_t));
+		if (temp == NULL)
+			return (NULL);
+
+		temp->n = n;
+		temp->next = NULL;
+		temp->prev = NULL;
+
+		ptr = *h;
+		while (i < idx)
+		{
+			ptr = ptr->next;
+			i++;
+		}
+		temp->next = ptr->next;
+		ptr->next->prev = temp;
+		ptr->next = temp;
+		temp->prev = ptr;
+	}
+	return (temp);
+}
