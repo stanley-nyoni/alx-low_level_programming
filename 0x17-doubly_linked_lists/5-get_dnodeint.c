fbe352c085d4c1bf6d7b07a9e0999a38f332c51b
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-08-18 21:01:11 +0000

    get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - get node at index
+* @head: head pointer
+* @index: nth node
+*
+* Return: node
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp = NULL;
+	unsigned int i = 0;
+	unsigned int count = 0;
+
+	count = dlistint_len(head);
+	if (index > count)
+		return (NULL);
+
+	while (i <= index)
+	{
+		if (i == index)
+			temp = head;
+		head = head->next;
+		i++;
+	}
+	return (temp);
+}
