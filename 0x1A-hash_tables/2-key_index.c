0e9545f7ed85d82104af2e7990876b5dc5cc8be0
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-09-08 20:07:16 +0000

    generates key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+* key_index - generates a distrubited index
+* @key: key
+* @size: table size
+*
+* Return: index
+*/
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	if (key == NULL || size == 0)
+	{
+		return (0);
+	}
+	return (hash_djb2(key) % size);
+}
